bc9e107499e05b2695dbab38e5de4fb86df6fce8
alnig441 <alnig441@gmail.com>
2016-07-22 15:24:20 -0500

    add & refresh timesheets functionality implemented, logout functionaliy corrected

--- app/app.php
+++ app/app.php
@@ -3,9 +3,13 @@
 session_id('iwscc');
 session_start();
 
-define(TS_URL, "https://radiant-cove-60089.herokuapp.com/api/v1/");
+define(BASE_URL, "https://radiant-cove-60089.herokuapp.com/api/v1/");
+define(LOGIN, "login");
+define(GET_ITEMS, "assignedItems/tickets/");
 
 require_once __DIR__.'/../vendor/autoload.php';
+include dirname(__FILE__).'/../src/services/getTimesheets.php';
+include dirname(__FILE__).'/../src/services/getItems.php';
 
 $app = new Silex\Application();
 
@@ -28,15 +32,9 @@ $app->post('/edit/{itemId}', function() use($app){
     return $app['twig']->render('editTicket.twig');
 });
 
-$app->post('/', function($loginUrl = 'login', $getItemsUrl = 'assignedItems/tickets/') use($app){
+$app->post('/', function() use($app){
     
     session_start();
-    
-    $login = TS_URL.$login;
-    
-//    ENTER CODE TO DISTINGUISH BETWEEN AUTHENTICATED AND NOT-AUTHENTICATED USER ...
-//    THEN SWITCH BETWEEN ADDING, EDITING OR DELETING
-//    ALTERNATIVELY SETUP DB CALLS AS A SERVICE AND CALL RELEVANT SERVICE IN THEIR SEPARATE ROUTES (ADD, EDIT, DELETE) ....
     
     if(empty($_POST['username']) && !empty($_POST['password'])){
         
@@ -45,14 +43,12 @@ $app->post('/', function($loginUrl = 'login', $getItemsUrl = 'assignedItems/tick
     }
     
     else{
-        
-        $loginUrl = TS_URL.$loginUrl;
 
         $curl = curl_init();
 
         $postOptions = array(
 
-            CURLOPT_URL => $loginUrl,
+            CURLOPT_URL => BASE_URL.LOGIN,
             CURLOPT_HEADER => true,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => false,
@@ -81,78 +77,19 @@ $app->post('/', function($loginUrl = 'login', $getItemsUrl = 'assignedItems/tick
                 
                 $_SESSION['userId'] = $user['userId'];
                 
+                preg_match_all('|Set-Cookie: (.*);|U', $data, $matches);   
+                $_SESSION['cookie'] = implode('; ', $matches[1]);
+                
             }
             
-            $getItemsUrl = TS_URL.$getItemsUrl.$_SESSION['userId'];
-
-            preg_match_all('|Set-Cookie: (.*);|U', $data, $matches);   
-            $cookies = implode('; ', $matches[1]);
-
-            $curl = curl_init();
-
-            $getTicketsOptions = array(
-
-                CURLOPT_URL => $getItemsUrl,
-                CURLOPT_FOLLOWLOCATION => false,
-                CURLOPT_HEADER => true,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_COOKIE => $cookies,
-            );
-
-            curl_setopt_array($curl, $getTicketsOptions);
-
-            $data = curl_exec($curl);
+            $tickets = getItems($_SESSION['userId']);
             
-            $split  = preg_split('|{|', $data);
-            $ticketStr = '';
-
-            foreach($split as $key => $value){
-                if($key != 0){
-                    $ticketStr = $ticketStr.'{'.$value;
-                }
-            }
-            
-            $tickets = json_decode($ticketStr, true);
-            
-            curl_close($curl);
-            
-//            BUILD ARRAY TO SEND TO TWIG TEMPLATE (EXISTING TIMESHEETS FROM TIMESHEET DB + ASSIGNED ITEMS FROM TICKET DB
-            
-            $con = mysqli_connect("localhost", "phpuser", "phpuserpw", "iws_cc");
-            if(!$con){
-                exit('Connect Error (' . mysqli_connect_errno() . ')'
-                        . mysqli_connect_error() );
-            }
-            mysqli_set_charset($con, 'utf-8');
-
-            $dbQuery = "SELECT * FROM timesheets WHERE userId = " . $_SESSION['userId'] ."";
-            
-            $iwsResult = mysqli_query($con, $dbQuery);
-            
-            if(mysqli_num_rows($iwsResult) < 1){
-
-                $timesheets = array(
-                    0 => array(
-                        'created' => null,
-                        'hours' => null,
-                        'ticket' => null,
-                        'comments' => null,
-                        'billable' => null
-                    )
-                );
-            }
-            
-            else {
-                $timesheets = mysqli_fetch_all($iwsResult, MYSQLI_ASSOC);
-            }
+            $timesheets = getTimesheet($_SESSION['userId']);
 
             $buildView = array(
                 'tickets' => $tickets,
                 'timesheets' => $timesheets,
              );
-            
-            mysqli_close($con);
-            mysqli_free_result($timesheets);
             
             return $app['twig']->render('viewTickets.twig', $buildView);
         }
@@ -163,11 +100,13 @@ $app->post('/', function($loginUrl = 'login', $getItemsUrl = 'assignedItems/tick
 $app->post('/addTimesheet', function() use($app){
     
     session_start();
+    
+    $userId = $_SESSION['userId'];
 
     $_POST['userId'] = $_SESSION['userId'];
-   
-    echo 'adding ticket'.json_encode($_POST).'<br/>';
     
+    echo 'week: ' . $_POST['week'] . '<br/>';
+   
     $con = mysqli_connect("localhost", "phpuser", "phpuserpw", "iws_cc");
             if(!$con){
                 exit('Connect Error (' . mysqli_connect_errno() . ')'
@@ -181,8 +120,18 @@ $app->post('/addTimesheet', function() use($app){
             
     if($iwsResult = mysqli_query($con, $dbQuery)){
         
-        echo 'updating ticket view ... <br/>';
+//            BUILD ARRAY TO SEND TO TWIG TEMPLATE (EXISTING TIMESHEETS FROM TIMESHEET DB + ASSIGNED ITEMS FROM TICKET DB
         
+        $timesheets = getTimesheet($_SESSION['userId']);
+
+        $tickets = getItems($_SESSION['userId']);
+
+        $buildView = array(
+            'tickets' => $tickets,
+            'timesheets' => $timesheets,
+         );
+
+        return $app['twig']->render('viewTickets.twig', $buildView);
     }
     
     else {
@@ -190,6 +139,9 @@ $app->post('/addTimesheet', function() use($app){
         echo 'check your form data';
         
     }
+    
+    mysqli_close($con);
+    mysqli_free_result($iwsResult);
     
     return $app['twig']->render('viewTickets.twig', $tickets);
 });
--- web/index.php
+++ web/index.php
@@ -11,10 +11,12 @@ and open the template in the editor.
         <title></title>
         <style>
             html {font-family: "Courier New", sans-serif, monospace;}
+            td{font-size: 14px;}
             table {border: 1px; width: 75%; text-align: center; white-space: nowrap;}
             thead, th {font-weight: normal; border: 1px solid;}
-            .table_text {white-space: nowrap; overflow: ellipsis; text-align: left;}
+            .table_text {white-space: nowrap; overflow: hidden; text-align: left;}
             .comments{width: 50%;}
+            .comments input{size:100%; width: 99%;}
             #logout {display: inline; position: absolute; right: 25%;}
             #datepicker {display: inline; position: absolute; left: 25%;}
         </style>   
